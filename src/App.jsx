792f8e5b2cfb0b48418008f2536c661547c02d25
mangala <ronaldotieno237@gmail.com>
2025-03-23 05:37:43 +0300

    Added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Home from "./components/Home";
 import Addbirthday from "./components/Addbirthday";
 
 function App () {
-  const [birthdays, setBirthdays] = useState([
-    {id: 1, name:'Lydia Lyminah', date:'1997-01-10'},
-    {id: 2, name:'Charity Mbula', date:'1997-01-23'},
-  ]);
+  const [birthdays, setBirthdays] = useState(() => {
+    const storedBirthdays = localStorage.getItem('birthdays');
+    return storedBirthdays ? JSON.parse(storedBirthdays) : [
+      {id: 1, name:'Lydia Lyminah', date:'1997-01-10'},
+      {id: 2, name:'Charity Mbula', date:'1997-01-23'},
+    ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('birthdays', JSON.stringify(birthdays));
+  }, [birthdays]);
 
   const addBirthday = (name, date) => {
     const newBirthday = { id: Date.now(), name, date};
